0cfebab6b3a454a2f4f4242f8be0659bb0d70af0
MohammadYahya <mohamadyahya979@gmail.com>
2022-07-17 13:25:47 -0700

    oop intro

---
+++ week2/Handelbar/main.js
@@ -0,0 +1,28 @@
+$("button").on("click", function () {
+  let address = $("#addr-input").val();
+  let minPrice = $("#min-p-input").val();
+  let maxPrice = $("#max-p-input").val();
+  let minRooms = $("#min-r-input").val();
+  let maxRooms = $("#max-r-input").val();
+  let immediate = $("#immed-y");
+
+  let relevantApts = findRelevantApts(
+    address,
+    minPrice,
+    maxPrice,
+    minRooms,
+    maxRooms,
+    immediate
+  );
+  renderApts(relevantApts);
+});
+
+const renderApts = function (apartments) {
+  $("#results").empty();
+  const source = $("#result-template").html();
+  const template = Handlebars.compile(source);
+  const newHTML = template({ apartments });
+  $("#results").append(newHTML);
+};
+
+renderApts(apartments); //renders apartments when page loads
